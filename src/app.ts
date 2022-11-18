bd3aac376f09b3a0a28dcb002349933e50f52208
hvbairagi <hvbairagi@gmail.com>
2022-11-18 22:34:31 +0530

    Completed Section 5

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,89 @@
-let userInput: unknown;
-let userName: string;
+class Department {
+  protected employees: string[] = [];
 
-userInput = 5;
-userInput = "Harsh";
-if (typeof userInput === "string") {
-  userName = userInput;
+  constructor(private id: string, public name: string) {}
+
+  describe(this: Department): void {
+    console.log("Department: ", this.name);
+  }
+
+  addEmployee(employee: string): void {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation(): void {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-function generateError(message: string, code: number): never {
-  throw { message, code };
+class ITDepartment extends Department {
+  public admins: string[];
+
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+  addEmployee(name: string): void {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
 }
 
-generateError("An error occurred!", 500);
+class AccountingDepartment extends Department {
+  private lastReport: string;
 
-console.log("Hello");
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found!");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string): void {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports(): void {
+    console.log(this.reports);
+  }
+}
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.mostRecentReport = "some value";
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+accounting.printReports();
+
+// const it = new ITDepartment("01", ["Max"]);
+
+// it.describe();
+// it.printEmployeeInformation();
