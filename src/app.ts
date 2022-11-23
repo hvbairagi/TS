d1a98595a0720b2326c7fdc2369008510ac5d228
hvbairagi <hvbairagi@gmail.com>
2022-11-23 22:39:17 +0530

    Completed Section 6

--- src/app.ts
+++ src/app.ts
@@ -1,89 +1,131 @@
-class Department {
-  protected employees: string[] = [];
+// Continue from Discriminated Unions
 
-  constructor(private id: string, public name: string) {}
+type Admin = { name: string; privileges: string[] };
 
-  describe(this: Department): void {
-    console.log("Department: ", this.name);
+type Employee = { name: string; startDate: Date };
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
   }
-
-  addEmployee(employee: string): void {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation(): void {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  return a + b;
 }
 
-class ITDepartment extends Department {
-  public admins: string[];
+const result = add("Harsh", "Bairagi");
+result.split(" ");
 
-  constructor(id: string, admins: string[]) {
-    super(id, "IT");
-    this.admins = admins;
-  }
+const fetchedUserData = {
+  id: "u1",
+  name: "Harsh",
+  job: { title: "CEO", description: "My own company" },
+};
 
-  addEmployee(name: string): void {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-}
+console.log(fetchedUserData?.job?.title);
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
+const userInput = undefined;
 
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found!");
-  }
+const storedData = userInput ?? "DEFAULT";
 
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value!");
-    }
-    this.addReport(value);
-  }
+// type UnknownEmployee = Employee | Admin;
 
-  constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//   console.log('Name: ' + emp.name);
+//   if ('privileges' in emp) {
+//     console.log('Privileges: ' + emp.privileges);
+//   }
+//   if ('startDate' in emp) {
+//     console.log('Start Date: ' + emp.startDate);
+//   }
+// }
 
-  addEmployee(name: string): void {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
+// printEmployeeInformation({ name: 'Manu', startDate: new Date() });
 
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
+// class Car {
+//   drive() {
+//     console.log('Driving...');
+//   }
+// }
 
-  printReports(): void {
-    console.log(this.reports);
-  }
-}
+// class Truck {
+//   drive() {
+//     console.log('Driving a truck...');
+//   }
 
-const accounting = new AccountingDepartment("d2", []);
+//   loadCargo(amount: number) {
+//     console.log('Loading cargo ...' + amount);
+//   }
+// }
 
-accounting.mostRecentReport = "some value";
-accounting.addReport("Something went wrong...");
-console.log(accounting.mostRecentReport);
+// type Vehicle = Car | Truck;
 
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
+// const v1 = new Car();
+// const v2 = new Truck();
 
-accounting.printReports();
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   if (vehicle instanceof Truck) {
+//     vehicle.loadCargo(1000);
+//   }
+// }
 
-// const it = new ITDepartment("01", ["Max"]);
+// useVehicle(v1);
+// useVehicle(v2);
 
-// it.describe();
-// it.printEmployeeInformation();
+// interface Bird {
+//   type: 'bird';
+//   flyingSpeed: number;
+// }
+
+// interface Horse {
+//   type: 'horse';
+//   runningSpeed: number;
+// }
+
+// type Animal = Bird | Horse;
+
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case 'bird':
+//       speed = animal.flyingSpeed;
+//       break;
+//     case 'horse':
+//       speed = animal.runningSpeed;
+//   }
+//   console.log('Moving at speed: ' + speed);
+// }
+
+// moveAnimal({type: 'bird', flyingSpeed: 10});
+
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// const userInputElement = document.getElementById('user-input');
+
+// if (userInputElement) {
+//   (userInputElement as HTMLInputElement).value = 'Hi there!';
+// }
+
+// interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character!' }
+//   [prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+//   email: 'Not a valid email!',
+//   username: 'Must start with a capital character!'
+// };
