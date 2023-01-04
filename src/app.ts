2838beb2eb7b7e5b51e73c519ebf6dae8922a19f
hvbairagi <hvbairagi@gmail.com>
2023-01-04 07:17:32 +0530

    my-code not working

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
+import "reflect-metadata";
+import {} from "class-transformer";
+import _ from "lodash";
+import { Product } from "./product.model";
 
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("finished");
+declare var GLOBAL: string;
+console.log(_.shuffle([1, 2, 3]));
+console.log(GLOBAL);
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A book", price: 10.99 },
+];
+
+const loadedProducts = products.map((prod) => {
+  return new Product(prod.title, prod.price);
+});
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// const p1 = new Product("A Book", 12.99);
+// console.log(p1.getInformation());
